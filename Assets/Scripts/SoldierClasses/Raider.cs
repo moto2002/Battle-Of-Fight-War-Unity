2515b9bb1a7db36d91ef2960a298f7750d663b7f
Thaw Htaik <thaw.htaik@gsw-w.com>
2013-11-12 17:08:42 -0500

    Added raider spawner prefab. Placed more enemies on border map

--- Assets/Scripts/SoldierClasses/Raider.cs
+++ Assets/Scripts/SoldierClasses/Raider.cs
@@ -8,7 +8,7 @@ public class Raider : BadSquadMember
 	public Raider(string name, float health = 100.0f) : base("Raider", name, health)
 	{
 		this.SquadViewTexture = Resources.Load ("Units/Raider") as Texture2D;
-		this.attackPower = 1.0f;
+		this.attackPower = 2.0f;
 	}
 	
 	
