4e63a042e4b1c0550364aebf3d82d6133bea89fd
Thaw Htaik <thaw.htaik@gmail.com>
2013-12-10 20:55:35 -0500

    Meta data bullshit

---
+++ Assets/Scripts/GUI/CommonGui.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommonGui : MonoBehaviour 
+{
+
+
+	public AudioClip Click1;
+	public AudioClip Click2;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
